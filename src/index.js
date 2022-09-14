587b1370027e326e0b99d9166aef22bec566641b
Will Paceley <pace0033@algonquinlive.com>
2022-09-14 10:06:02 -0400

    add ability to get people from firestore and update DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBq5jD2uud2icF5lXZ13LAcGr9zvk_v75Q",
@@ -7,41 +7,96 @@ const firebaseConfig = {
   projectId: "fire-giftr-17910",
   storageBucket: "fire-giftr-17910.appspot.com",
   messagingSenderId: "953551576185",
-  appId: "1:953551576185:web:28a3e5fbc53d5d733a08a8"
+  appId: "1:953551576185:web:28a3e5fbc53d5d733a08a8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // get a reference to the database
 const db = getFirestore(app);
+// App constants
+const people = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-  //set up the dom events
-  document
-    .getElementById('btnCancelPerson')
-    .addEventListener('click', hideOverlay);
-  document
-    .getElementById('btnCancelIdea')
-    .addEventListener('click', hideOverlay);
-  document.querySelector('.overlay').addEventListener('click', hideOverlay);
-
-  document
-    .getElementById('btnAddPerson')
-    .addEventListener('click', showOverlay);
-  document.getElementById('btnAddIdea').addEventListener('click', showOverlay);
+document.addEventListener("DOMContentLoaded", async () => {
+  // TODO: overlay currently active by default, remove later
+  document.querySelector(".overlay").classList.remove("active");
+  addClickListeners();
+  // Get people from Firestore
+  await getPeople();
 });
 
+function addClickListeners() {
+  //set up the dom events
+  document
+    .getElementById("btnCancelPerson")
+    .addEventListener("click", hideOverlay);
+  document
+    .getElementById("btnCancelIdea")
+    .addEventListener("click", hideOverlay);
+  document.querySelector(".overlay").addEventListener("click", hideOverlay);
+
+  document
+    .getElementById("btnAddPerson")
+    .addEventListener("click", showOverlay);
+  document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
+}
 function hideOverlay(ev) {
   ev.preventDefault();
-  document.querySelector('.overlay').classList.remove('active');
+  document.querySelector(".overlay").classList.remove("active");
   document
-    .querySelectorAll('.overlay dialog')
-    .forEach((dialog) => dialog.classList.remove('active'));
+    .querySelectorAll(".overlay dialog")
+    .forEach((dialog) => dialog.classList.remove("active"));
 }
 function showOverlay(ev) {
   ev.preventDefault();
-  document.querySelector('.overlay').classList.add('active');
-  const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
+  document.querySelector(".overlay").classList.add("active");
+  const id = ev.target.id === "btnAddPerson" ? "dlgPerson" : "dlgIdea";
   //TODO: check that person is selected before adding an idea
-  document.getElementById(id).classList.add('active');
+  document.getElementById(id).classList.add("active");
+}
+
+/* --- PEOPLE FUNCTIONS --- */
+async function getPeople() {
+  const collectionRef = collection(db, "people");
+  const peopleSnapshot = await getDocs(collectionRef);
+
+  // Populate people array with database results
+  peopleSnapshot.forEach((person) => {
+    const id = person.id;
+    const data = person.data();
+    people.push({ id, ...data });
+  });
+
+  buildPeople(people);
+}
+
+function buildPeople(peopleArray) {
+  const ul = document.querySelector(".person-list");
+  const months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  ul.innerHTML = people
+    .map((person) => {
+      const birthMonth = months[person["birth-month"] - 1];
+      const birthDay = person["birth-day"];
+      const dob = `${birthMonth} ${birthDay}`;
+
+      return `<li data-id=${person.id} class="person">
+      <p class="name">${person.name}</p>
+      <p class="dob">${dob}</p>
+    </li>`;
+    })
+    .join("");
 }
